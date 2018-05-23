86551e7469fc1c80edd02328ad6b1671572def17
Daimon01 <Dimas.1990@inbox.ru>
2018-05-23 14:14:51 +0600

    sql

---
+++ example/LessonArt/src/main/java/com/jdbc/Main.java
@@ -0,0 +1,46 @@
+package com.jdbc;
+
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        Driver drv;
+
+        try {
+            drv = new com.mysql.cj.jdbc.Driver();
+        } catch (SQLException ex) {
+            System.out.println("Произошла ошибка при создании драйвера");
+            return;
+        }
+        try {
+            DriverManager.registerDriver(drv);
+        } catch (SQLException ex) {
+            System.out.println("Неудалось зарегистрировать драйвер");
+            return;
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("code.....");
+        }
+        catch (SQLException ex){
+            System.out.println("Неудалось соединение");
+            return;
+        }
+finally {
+            if (connection != null){
+                connection.close();
+            }
+        }
+
+    }
+}
